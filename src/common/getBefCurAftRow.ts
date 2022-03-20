8c3404dcb2de32717e364224eeed144f026a5919
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-03-20 17:09:42 +0000

    refact

--- src/common/getBefCurAftRow.ts
+++ src/common/getBefCurAftRow.ts
@@ -1,5 +1,4 @@
-import { gameState } from "../app.js";
-import { Field, GameFieldMap, RowObj } from "../interfaces";
+import { Field, GameFieldMap, RowObj } from '../interfaces';
 
 export const getBefCurAftRow = (
   rowIndex: number,
@@ -7,18 +6,10 @@ export const getBefCurAftRow = (
 ): Array<RowObj> => {
   const rows: Array<RowObj> = [];
   for (let i = -1; i < 2; i++) {
-    const row: RowObj | undefined = gameField[rowIndex + i]
-      ? { rowIndex: rowIndex + i, row: gameField[rowIndex + i] }
-      : undefined;
-    if (row) {
-      rows.push(row);
+    if (gameField[rowIndex + i]) {
+      rows.push({ rowIndex: rowIndex + i, row: gameField[rowIndex + i] });
     }
   }
-  // const rows = [
-  //   gameField[rowIndex - 1] ? gameField[rowIndex - 1] : null,
-  //   gameField[rowIndex],
-  //   !gameField[rowIndex + 1] ? gameField[rowIndex + 1] : null,
-  // ].filter((row) => row) as <Array<Array<Field>>
 
   return rows;
 };
