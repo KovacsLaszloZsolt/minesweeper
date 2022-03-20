1c27146f9826e7693637f8a4d568223d95257237
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-03-20 18:04:56 +0000

    refactor

--- src/game/getPartFieldMap.ts
+++ src/game/getPartFieldMap.ts
@@ -1,31 +1,15 @@
-import { GameFieldMap, PartRowObj, Field, RowField } from "../interfaces";
+import { GameFieldMap, PartRowObj } from '../interfaces';
+import { getBefCurAftRow } from '../common/getBefCurAftRow.js';
+import { getPartRows } from './getPartRows.js';
 
 export const getPartFieldMap = (
   rowIndex: number,
   index: number,
   gameField: GameFieldMap
 ): Array<PartRowObj> => {
-  const rows: Array<PartRowObj> = [];
-  for (let i = -1; i < 2; i++) {
-    const row: Array<Field> | undefined = gameField[rowIndex + i]
-      ? gameField[rowIndex + i]
-      : undefined;
-    if (row) {
-      const partRowObj: PartRowObj = {
-        rowIndex: rowIndex + i,
-        rowFields: [],
-      };
-      for (let i = -1; i < 2; i++) {
-        if (row[index + i]) {
-          const field: RowField = {
-            index: index + i,
-            field: row[index + i],
-          };
-          partRowObj.rowFields.push(field);
-        }
-      }
-      rows.push(partRowObj);
-    }
-  }
+  const fullRows = getBefCurAftRow(rowIndex, gameField);
+  console.log('getBefCurAftRow:', fullRows);
+
+  const rows: Array<PartRowObj> = getPartRows(index, fullRows);
   return rows;
 };
