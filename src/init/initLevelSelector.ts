3651beec49a5e8e99b4358289c0d8e6982a3d228
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-03-21 10:59:33 +0000

    feat: add renderRemainingFlags

--- src/init/initLevelSelector.ts
+++ src/init/initLevelSelector.ts
@@ -1,17 +1,23 @@
-import { gameState } from '../app.js';
+import { gameState, levelOptions } from '../app.js';
+import { LevelOptions } from '../interfaces.js';
 import { clearBoard } from '../render/clearBoard.js';
 import { render } from '../render/render.js';
+import { renderRemainingFlags } from '../render/renderRemainingFlags.js';
 import { initGame } from './initGame.js';
 export const initLevelSelector = () => {
   const levelSelector = document.querySelector(
     '.level-selector'
   ) as HTMLSelectElement;
   levelSelector.addEventListener('change', (e: Event) => {
+    if (gameState.gameStarted) {
+      gameState.gameStarted = false;
+    }
     gameState.level = levelSelector.value;
-    console.log(levelSelector.value);
-    console.log(gameState.level);
+    gameState.numOfFlagsLeft =
+      levelOptions[gameState.level as keyof LevelOptions].numOfMines;
     gameState.gameFieldsMap = [];
     clearBoard();
     initGame();
+    renderRemainingFlags(gameState.numOfFlagsLeft);
   });
 };
