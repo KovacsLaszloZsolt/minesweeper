6b36cddcb23dae81d1487967e9e21ae78dc4a23e
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2022-03-21 10:54:15 +0000

    refact

--- src/init/initGame.ts
+++ src/init/initGame.ts
@@ -2,12 +2,13 @@ import { gameState, levelOptions } from '../app.js';
 import { createGameFieldsMap } from '../game/createGameFieldsMap.js';
 import { LevelOptions } from '../interfaces';
 import { render } from '../render/render.js';
+import { renderRemainingFlags } from '../render/renderRemainingFlags.js';
 export const initGame = () => {
   const level: string = gameState.level;
-  console.log(levelOptions[level as keyof LevelOptions]);
   gameState.gameFieldsMap = createGameFieldsMap(
     levelOptions[level as keyof LevelOptions],
     gameState.gameFieldsMap
   );
   render();
+  renderRemainingFlags(gameState.numOfFlagsLeft);
 };
